a5aed19b110a8989dea76bd941acda15d8081426
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2020-08-21 18:46:12 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+//question 1
+
+const button1 = document.querySelector("#bg");
+
+button1.onclick = function () {
+	//console.log(event.target);
+	//event.target.style.background = "green";
+	document.body.style.background = "green";
+};
+
+//question 2
+
+const button2 = document.querySelector(".toggler");
+const togglee = document.querySelector(".togglee");
+
+button2.onclick = function () {
+	//console.log(event.target);
+	//event.target.classList.add("extra");
+	//event.target.classList.toggle("extra");
+	togglee.classList.toggle("extra");
+};
+
+//question 3
+
+const dropdown = document.querySelector("select");
+const paragraphs = document.querySelector(".paragraphs-container");
+const content = document.querySelector(".content");
+dropdown.onchange = function () {
+	console.log(event.target.value);
+	const selectedValue = this.value;
+
+	paragraphs.innerHTML = "";
+	for (let i = 1; i <= selectedValue; i++) {
+		paragraphs.innerHTML += `<div class="content"></div>`;
+	}
+};
+
+//question 4
+
+const text = document.querySelector(".input");
+const characterCount = document.querySelector(".char-count");
+
+text.onkeyup = function () {
+	console.log(event.target.value.length);
+
+	const len = event.target.value.length;
+
+	characterCount.innerHTML = len;
+};
+
+//question 5
+
+const body = document.querySelector("body");
+
+function handleScroll() {
+	console.log(window.scrollY);
+	const highlight = window.scrollY;
+
+	if (highlight > 35) {
+		document.body.classList.add("highlight");
+		body.classList.add("highlight");
+	} else {
+		document.body.classList.remove("highlight");
+		body.classList.remove("highlight");
+	}
+}
+
+window.onscroll = handleScroll;
+//window.onscroll = function () {
+//	console.log("scrolling");
+//};
